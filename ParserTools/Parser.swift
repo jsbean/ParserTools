ed22b24ce5afffd2885b5ed1121fdf529b0e07db
jsbean <jbean46@gmail.com>
2016-08-21 18:29:31 -0400

    Add public initialize for Parser

--- ParserTools/Parser.swift
+++ ParserTools/Parser.swift
@@ -16,6 +16,15 @@ public struct Parser<Token, Result> {
     /// Function that takes an `ArraySlice` of generic Token type, and returns a sequence of
     /// 2-tuples containing the generic Result type and an `ArraySlice` of Token type values.
     public let parse: ArraySlice <Token> -> AnySequence <(Result, ArraySlice<Token>)>
+    
+    // MARK: - Initializers
+    
+    /**
+     Create a `Parser` with a given `parse` function.
+     */
+    public init(_ parse: ArraySlice <Token> -> AnySequence <(Result, ArraySlice<Token>)>) {
+        self.parse = parse
+    }
 }
 
 public func eof <A> () -> Parser<A, ()> {
